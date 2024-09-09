1c48cc3d318ba69c74e0dec92c6f8a2b9b1a3e30
Lucas Spitzer <lucasspitzer30@gmail.com>
2024-09-09 23:32:34 +0200

    app

--- SELECTEUR_RGB_XAML/CADRE.xaml.cs
+++ SELECTEUR_RGB_XAML/CADRE.xaml.cs
@@ -9,24 +9,18 @@ namespace RGB_XAML
         {
             InitializeComponent();
         }
-
-        // Méthode qui gère le changement de couleur
         private void OnColorChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            // Récupérer les valeurs des sliders
             int red = (int)SliderRouge.Value;
             int green = (int)SliderVert.Value;
             int blue = (int)SliderBleu.Value;
 
-            // Afficher les valeurs actuelles des sliders dans les TextBlock
             RougeValue.Text = red.ToString();
             VertValue.Text = green.ToString();
             BleuValue.Text = blue.ToString();
 
-            // Créer une nouvelle couleur basée sur les valeurs des sliders
             Color color = Color.FromRgb((byte)red, (byte)green, (byte)blue);
 
-            // Mettre à jour le fond du carré de couleur
             CouleurPreview.Background = new SolidColorBrush(color);
         }
     }
